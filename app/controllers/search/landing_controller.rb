1f879d6888b417e266b4455147375aaa987b146e
W!ze Flux <fear.ru@gmail.com>
2013-04-30 22:28:29 +0800

    CSS fixes

--- app/controllers/search/landing_controller.rb
+++ app/controllers/search/landing_controller.rb
@@ -7,17 +7,24 @@ class Search::LandingController < ApplicationController
   
   def index
     @events = Event.
+                full!.
                 page(1).
                 per(7).
                 order_by_date.
                 full_text_search(query_text)
+                
     @event_groups = EventGroup.
                       page(1).
                       per(5).
                       full_text_search(query_text)
+                      
     @venues = Venue.
                 page(1).
                 per(5).
                 full_text_search(query_text)
+                
+    @events_counter = Event.full_text_search(query_text).count                        
+    @event_groups_counter = EventGroup.full_text_search(query_text).count
+    @venues_counter = Venue.full_text_search(query_text).count
   end
 end
