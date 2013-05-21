ed500eabb17d4e0ee4de794edbc4a053d7c666fe
W!ze Flux <fear.ru@gmail.com>
2013-05-21 21:03:39 +0800

    unicorn tuneup

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,11 +1,11 @@
 rails_root = `pwd`.gsub("\n", "")
 
 worker_processes 2
-preload_app true
+preload_app false
 timeout 30
 working_directory rails_root
 
-listen "#{rails_root}/tmp/sock/unicorn.sock", :backlog => 64, :tcp_nopush => false
+listen "#{rails_root}/tmp/sock/unicorn.sock", :backlog => 128, :tcp_nopush => false
 pid "#{rails_root}/tmp/pids/unicorn.pid"
 
 stderr_path "#{rails_root}/log/unicorn.stderr.log"
